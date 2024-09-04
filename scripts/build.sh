2b5894700cd4ecbf9ccbf2faa5ba2509ea566565
mavericb <eric.boniardi@mail.polimi.it>
2024-09-04 10:53:42 -0600

    .

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,20 +3,22 @@
 # This script is used to build all of the modules in SDXL Pipeline.
 # It requires *quite a lot* of disk space - be warned!
 
-### VERSIONS ###
-
-### NOTE ###
-# Specify the versions of the Docker and Lilypad modules in VERSIONS.env
-
 # Change to the directory that this script is in.
 cd "$(dirname "$0")"
 
-# Load the versions
-source VERSIONS.env
+# Load the configuration
+source CONFIG.env
 
-# Check that the Docker versions are set
-if [[ -z $VLLAMA3_8B ]]; then
-    echo "Please set the Docker versions in VERSIONS.env before building."
+# Check if Docker username and password are provided as arguments
+if [ $# -eq 2 ]; then
+    DOCKER_USERNAME=$1
+    DOCKER_PASSWORD=$2
+fi
+
+# Check that the required variables are set
+if [[ -z $DOCKER_USERNAME || -z $DOCKER_PASSWORD || -z $IMAGE_NAME ]]; then
+    echo "Usage: $0 <DOCKER_USERNAME> <DOCKER_PASSWORD>"
+    echo "Or set DOCKER_USERNAME, DOCKER_PASSWORD, and IMAGE_NAME in CONFIG.env before building."
     exit 1
 fi
 
@@ -28,17 +30,16 @@ cd ../docker/
 export DOCKER_BUILDKIT=1
 
 # Login to Docker Hub
-docker login -u mavericb -p X
-
-# Build the Docker image
-docker build -f Dockerfile-llama3-8b -t mavericb/ollama:llama3-8b-lilypad$VLLAMA3_8B .
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
 # Generate a new tag
 NEW_TAG=$(date +v%Y%m%d%H%M%S)
-docker build -f Dockerfile-llama3-8b -t mavericb/ollama:llama3-8b-lilypad-$NEW_TAG .
+
+# Build the Docker image
+docker build -f Dockerfile -t $DOCKER_USERNAME/$IMAGE_NAME:$NEW_TAG .
 
 # Publish the Docker containers
 echo "Publishing Docker containers..."
-docker push mavericb/ollama:llama3-8b-lilypad-$NEW_TAG
+docker push $DOCKER_USERNAME/$IMAGE_NAME:$NEW_TAG
 
 echo "Done!"
